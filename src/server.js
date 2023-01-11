9e7590e8fc637687e307a120d68404f29ed1ea9e
carlos <carloslonghi.cl@gmail.com>
2023-01-11 11:25:11 -0300

    uploading project

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
-const express = require('express') // Importing express 
-const app = express() // Starting express 
-const PORT = 3333 // Route express
+// Importing express
+const express = require('express')
+// Starting express
+const app = express()
+
+// Params são utilizados para passar dados simples. como: id, name, etc...
+app.get('/message/:id_product/:name_product', (request, response) => {
+  // Desestruturando params:
+  const { id_product, name_product } = request.params
+  response.send(`
+  Id do produto: ${id_product}. <br>
+  Nome do produto: ${name_product}.
+  `)
+})
+
+// Route express
+const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
