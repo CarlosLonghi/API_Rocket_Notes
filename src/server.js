008b7437182cc2f4f4d095ae7291bc4748914baf
carlos <carloslonghi.cl@gmail.com>
2023-01-11 15:07:48 -0300

    feat: structural organization

--- src/server.js
+++ src/server.js
@@ -1,29 +1,13 @@
 // Importing express
 const express = require('express')
+
+const routes = require('./routes') // Carrega o index.js
+
 // Starting express
 const app = express()
-
-// (Route Params ':/params') são utilizados para passar dados simples. como: id, name, etc...
-app.get('/message/:id_product/:name_product', (request, response) => {
-  // Desestruturando params:
-  const { id_product, name_product } = request.params
-  response.send(`
-  Id do produto: ${id_product}. <br>
-  Nome do produto: ${name_product}.
-  `)
-})
-
-// (Query Params)Exemple:'users?page=5&category=10'
-
-app.get('/users', (request, response) => {
-  const { page, category } = request.query
-  response.send(`
-  Número da Página: ${page}. <br>
-  Categoria do produto: ${category}.
-  `)
-})
+app.use(express.json())
+app.use(routes)
 
 // Route express
 const PORT = 3333
-
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`))
