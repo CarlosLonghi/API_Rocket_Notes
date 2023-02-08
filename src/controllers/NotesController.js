bfa008b24f02bc00a75b50c8137bc6ed5a6d4e76
carlos <carloslonghi.cl@gmail.com>
2023-02-08 14:54:07 -0300

    feat
    Create Delete function in Class NotesController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -49,7 +49,12 @@ class NotesController {
     })
   }
 
-  
+  async delete(request, response) {
+    const { id } = request.params
+
+    await knex('notes').where({ id }).delete()
+    return response.json()
+  }
 }
 
 module.exports = NotesController
